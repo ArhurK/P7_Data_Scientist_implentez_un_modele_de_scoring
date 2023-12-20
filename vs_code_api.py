ebb8db51975433606a69be8c2252bb382f28c762
ArhurK <96179590+ArhurK@users.noreply.github.com>
2023-12-20 18:45:47 +0100

    Add files via upload

--- vs_code_api.py
+++ vs_code_api.py
@@ -11,6 +11,7 @@ df = pd.read_pickle('test_df.pkl')
 from flask import Flask, request, jsonify
 import pandas as pd
 import mlflow.sklearn
+import shap
 
 app = FastAPI()
 
@@ -19,7 +20,7 @@ def Hello():
     return {'Hello ceci est un test' : 'test 1'}
 
 @app.get('/predict_proba') # get pas post
-def predict_proba(id_client = None):
+def predict_proba(id_client : dict):
     
     # Charger les données du formulaire
     #     data = request.json
@@ -27,6 +28,7 @@ def predict_proba(id_client = None):
     # df_test = pd.read_pickle('test_df.pkl')
     
     # Sélection ID client
+    print(id_client)
     try:
         selected_row = df.loc[df['SK_ID_CURR'] == id_client['index']]
     except IndexError:
@@ -37,14 +39,40 @@ def predict_proba(id_client = None):
     
     # Sélection des features
     feats = [f for f in df.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
-    X = df[feats]
+    X = selected_row[feats]
 
     # Effectuer la prédiction
     predictions = model.predict_proba(X)
+    print(predictions)
 
     # Convertir les prédictions en format JSON
     # Faire plutot f('prediction{id_client}')
     result = {'predictions': predictions.tolist()}
 
-    return jsonify(result)
+    return result
 
+@app.get('/shap')
+def shap_vector(id_client : dict):
+
+    try:
+        selected_row = df.loc[df['SK_ID_CURR'] == id_client['index']]
+    except IndexError:
+        print('ID Not found')
+    else:
+        # Acces à la ligne sélectionnée
+        print(selected_row)
+    
+    # Sélection des features
+    feats = [f for f in df.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
+    X = selected_row[feats]
+
+    # Effectuer la prédiction
+    predictions = model.predict_proba(X)
+
+    # Calculer les Shapley values
+    explainer = shap.TreeExplainer(model)
+    # Convertir le DataFrame X en une matrice NumPy
+    X_array = X.values
+    shap_values = explainer.shap_values(X_array)
+    shap_values_dict = {'shap_values' : shap_values}
+    return shap_values_dict
